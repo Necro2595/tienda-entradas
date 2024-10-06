5e15e18f0c55b90b2a10523650677ccb1156690c
Necro2595 <xavier2595@gmail.com>
2024-10-06 16:21:58 +0200

    migracion angular core y cli 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,32 +10,24 @@ import { CarritoComponent } from './carrito/carrito.component';
 import { BannerComponent } from './banner/banner.component';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatBadgeModule } from '@angular/material/badge';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    PaginaInicialComponent,
-    TiendaComponent,
-    CarritoComponent,
-    BannerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    CommonModule,
-    HttpClientModule,
-    MatIconModule,
-    MatCardModule,
-    MatButtonModule,
-    MatBadgeModule
-
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        PaginaInicialComponent,
+        TiendaComponent,
+        CarritoComponent,
+        BannerComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        CommonModule,
+        MatIconModule,
+        MatCardModule,
+        MatButtonModule,
+        MatBadgeModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
