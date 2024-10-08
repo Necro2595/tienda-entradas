dfdd72ed0e398145204e7c018a8ba2a557471822
Necro2595 <xavier2595@gmail.com>
2024-10-08 20:22:34 +0200

    arreglamos las fechas de la página principal

--- src/app/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pagina-inicial/pagina-inicial.component.ts
@@ -68,7 +68,7 @@ export class PaginaInicialComponent implements OnInit {
 
   formatDate(timestamp:number){
 
-    const date = new Date(timestamp);
+    const date = new Date(+timestamp);
     const day = ('0' + date.getDate()).slice(-2);
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
     const year = date.getFullYear();
